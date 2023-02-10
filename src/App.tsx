e51a1e198016cec54ce13ced4fb6efb5176e621d
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-02-09 22:22:54 -0300

    fix header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Header } from "./components/Header";
-
+import styles from "./App.module.scss";
 export function App() {
   return (
     <article>
       <Header />
+      <div className={styles.tableTask}>
+        <div className={styles.headerTable}>
+          <h1 className={styles.createdTask}>
+            Tarefas criadas <span>0</span>
+          </h1>
+          <h1 className={styles.completedTask}>
+            Concluídas <span>0</span>
+          </h1>
+        </div>
+      </div>
     </article>
   );
 }
