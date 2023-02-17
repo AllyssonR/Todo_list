0b4870d610a3e6fe5540f200463a601783c8b8f3
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-02-17 18:01:48 -0300

    Update Task.tsx

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,9 +1,23 @@
 import { Trash } from "phosphor-react";
+import { useState } from "react";
 import styles from "./Task.module.scss";
 export function Task() {
+  const [isTaskDone, setIsTaskDone] = useState(false);
+  function onTaskDone(event: any) {
+    if (event.target.value === false) {
+      setIsTaskDone(true);
+    } else {
+      setIsTaskDone(false);
+    }
+  }
   return (
     <li className={styles.content}>
-      <input type="checkbox" name="isTashDone" id="isTaskDone" />
+      <input
+        type="checkbox"
+        name="isTashDone"
+        id="isTaskDone"
+        onClick={onTaskDone}
+      />
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam
         voluptatum sed nemo aspernatur deleniti soluta maiores architecto
