fa2fdd35ef95f0cdf4aafaa2557a98e0cf3b3b73
Allysson Ribeiro <allyssonribeiro1@gmail.com>
2023-03-22 08:14:19 -0300

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { NoTask } from "./components/NoTask";
 import styles from "./App.module.scss";
 import { Task } from "./components/Task";
 import { v4 as uuidV4 } from "uuid";
-import { FormEvent } from "react";
-interface TaskProps {
+import { FormEvent, useState } from "react";
+export interface TaskProps {
   id: string;
   isDone: boolean;
   task: string;
 }
 
+/*
 const tasks = [
   { id: uuidV4(), isDone: true, text: "limpar o Quarto" },
   { id: uuidV4(), isDone: false, text: "limpar a casa" },
@@ -17,14 +18,16 @@ const tasks = [
   { id: uuidV4(), isDone: false, text: "regar as plantas" },
   { id: uuidV4(), isDone: false, text: "consertar o telhado" },
 ];
+*/
 
 export function App() {
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
   function handleNewTask(event: FormEvent) {
     event.preventDefault();
   }
   return (
     <article className={styles.mainPage}>
-      <Header />
+      <Header setTasks={setTasks} />
       <div className={styles.tableTask}>
         <div className={styles.headerTable}>
           <h1 className={styles.createdTask}>
